b9253653791ee8cb6347564c7bed8694345f2e4a
edgar-macias <edgarmacias@aragon.unam.mx>
2025-02-03 17:43:57 -0600

    Fix: We were changed the wrong controller to normalize or follow the symfony standard.

--- src/Controller/WrongController.php
+++ src/Controller/WrongController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/wrong', name: 'wrong_')]
 final class WrongController extends AbstractController
 {
-    #[Route('/wrong', name: 'app_wrong')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('wrong/index.html.twig', [
             'controller_name' => 'WrongController',
         ]);
     }
+
 }
